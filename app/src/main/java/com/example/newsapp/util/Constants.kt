671feb5eb1e7b39af45893d3d3b88eb44598d823
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-08-29 20:27:50 +0700

    change API

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "pub_519542b3c4a1e00213466fe25e99ead2fdfd2"
+        const val API_KEY = "pub_520414f8497c83c508c64a9452a2a516759ea"
         const val BASE_URL = "https://newsdata.io/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
